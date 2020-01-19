3e6cfcfec6c57adedcd509470865d81a861b6e47
kangalioo <jannik.a.schaper@web.de>
2020-01-19 16:33:07 +0100

    Move .mc specific code from chart.py to new mc.py

--- main.py
+++ main.py
@@ -1,34 +1,15 @@
-import util
-from convert import *
-
-"""
-TODO:
-- Maybe render keysounds
-- Look at data.zip not being extracted
-- More escaping! Especially # escaping in .sm meta tags
-
-Done:
-- Limit snap to 192nd
-- Offsync problems! E.g. in Accelerator and that other chart I played
-- Better folder naming scheme
-"""
+from glob import iglob
+import os
+import util, sm, chart, mc
 
 def main():
-	malody_source_dir = "malody-dump/"
-	# ~ malody_source_dir = "../download-malody-charts/output"
+	mc_paths = iglob("malody-dump/*/*/*.mc")
+	mc_path = next(mc_paths)
 	
-	for keymode in [4]:
+	library = chart.Library()
+	mc.parse(library, mc_path)
 	
-		output_dir = f"output/Malody {keymode}k Converts"
-		
-		# ~ print("Parsing charts...")
-		# ~ library = build_library(malody_source_dir, limit=None, keymode_filter=keymode)
-		# ~ library.print_stats()
-		
-		# ~ print("Writing charts as .sm...")
-		# ~ assemble_sm_pack(library, output_dir, separate_charts=True)
-		
-		analyze_msd(output_dir, "output")
+	print(sm.gen_sm(library.songs[0]))
 
 if __name__ == "__main__":
 	main()
